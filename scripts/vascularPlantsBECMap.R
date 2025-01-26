22c9e00668ff66b0527b4c5f5737c1cba9e4a71b
chlorophilia <andrewsimon@MacBook-Air-474.lan>
2025-01-25 18:40:10 -0700

    updated vascularPlantsBECMap.R to visualize vascular plant data by BEC Zone; added new inputs and generated new outputs; deleted old files not relevant for 2024 visualizations

--- scripts/vascularPlantsBECMap.R
+++ scripts/vascularPlantsBECMap.R
@@ -1,5 +1,9 @@
 # Visualize vascular plant diversity in Átl’ka7tsem by BEC unit
 
+# Set relative paths (https://stackoverflow.com/questions/13672720/r-command-for-setting-working-directory-to-source-file-location-in-rstudio)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
+
 # Load libraries
 
 library(dplyr)
@@ -14,24 +18,25 @@ library(htmlwidgets)
 
 # Source dependencies
 
-source("scripts/utils.R")
+source("../scripts/utils.R")
 
-# Call map layers
+# Call tabular data
 
-# Layer 1: BEC zone x vascular plants
-BEC <- mx_read("spatial_data/vectors/BEC")
+BEC.x.plants <- read.csv("../tabular_data/BEC_x_vascular_plants_summary_2024.csv")
 
-# Layer 2: BEC zones
-plants.x.BEC <- mx_read("spatial_data/vectors/BEC_x_plants_2024")
+# Call spatial data
+
+# Layer 1: BEC zones
+BEC <- mx_read("../spatial_data/vectors/BEC")
 
 # Layer 2: hillshade raster
-hillshade <- raster("spatial_data/rasters/Hillshade_80m.tif")
+hillshade <- raster("../spatial_data/rasters/Hillshade_80m.tif")
 
 # Layer 3: coastline
-coastline <- mx_read("spatial_data/vectors/Islands_and_Mainland")
+coastline <- mx_read("../spatial_data/vectors/Islands_and_Mainland")
 
 # Layer 4: watershed boundary
-watershed.boundary <- mx_read("spatial_data/vectors/Howe_Sound")
+watershed.boundary <- mx_read("../spatial_data/vectors/Howe_Sound")
 
 # Create map labels
 
@@ -50,28 +55,31 @@ becLabels <- list(CMAunp = "Coastal Mountain Heather Alpine Zone",
 # Following rough elevational gradient:  
 # CDFmm, CWHxm1, CWHdm, CWHvm1, CWHvm2, CWHds1, CWHms1, MHmm1, MHmm2, ESSFmw2, CMAunp
 
-plants.x.BEC$MAP_LAB <- as.factor(plants.x.BEC$MAP_LAB)
-
 palette = data.frame(
   cat = c("CWHxm1","CWHdm","CWHvm2","CWHvm1","CWHds1","CWHms1","MHmm1","MHmm2","CMAunp"), 
   col = c("#440154FF", "#472D7BFF","#3B528BFF","#2C728EFF","#21908CFF","#27AD81FF","#5DC863FF","#AADC32FF", "#FDE725FF")
 )
 
 # Reduce plants.x.BEC dataframe for visualization
-taxa <- plants.x.BEC %>%
-  group_by(MAP_LAB) %>%
-  summarize(taxa = paste(scntfcN, collapse = ", "), .groups = "drop")
+
+BEC.x.plants$MAP_LABEL <- as.factor(BEC.x.plants$MAP_LABEL)
+
+taxa <- BEC.x.plants %>%
+  group_by(MAP_LABEL) %>%
+  summarize(taxa = paste(scientificName, collapse = ", "), .groups = "drop")
+
+# Generate visualization data and aesthetics
 
 paletteHash <- split(x = palette$col, f = palette$cat)
-taxaHash <- split(x = taxa$taxa, f = taxa$MAP_LAB)
+taxaHash <- split(x = taxa$taxa, f = taxa$MAP_LABEL)
 
 vascularData <- list(palette = paletteHash, taxa = taxaHash, mapTitle = "Map 1. Ecological communities", regionLabels = becLabels)
 
-taxa <- base::merge(taxa, palette, by.x ="MAP_LAB", by.y="cat")
+BEC <- base::merge(BEC, palette, by.x ="MAP_LABEL", by.y="cat")
 
-# Write summarised plants to JSON file for viz
+# Write plant x BEC Zone summary to JSON file for viz
 
-write(jsonlite::toJSON(vascularData, auto_unbox = TRUE, pretty = TRUE), "viz_data/Vascular-plotData.json")
+write(jsonlite::toJSON(vascularData, auto_unbox = TRUE, pretty = TRUE), "../viz_data/Vascular-plotData.json")
 
 # Plot map
 
@@ -80,7 +88,7 @@ speciesMap <- leaflet(options=list(mx_mapId="Vascular")) %>%
   addTiles(options = providerTileOptions(opacity = 0.5)) %>%
   addRasterImage(hillshade, opacity = 0.8) %>%
   addPolygons(data = coastline, color = "black", weight = 1.5, fill = FALSE) %>%
-  addPolygons(data = plants.x.BEC, fillColor = plants.x.BEC$col, fillOpacity = 0.6, weight = 2, stroke = FALSE, options = labelToOption(BEC$MAP_LABEL)) %>%
+  addPolygons(data = BEC, fillColor = BEC$col, fillOpacity = 0.6, weight = 2, stroke = FALSE, options = labelToOption(BEC$MAP_LABEL)) %>%
   addPolygons(data = watershed.boundary, color = "black", weight = 4, fill = FALSE)
 
 #Note that this statement is only effective in standalone R
